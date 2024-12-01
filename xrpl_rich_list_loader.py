caaf3e45b1fe0c76f55227c29deae87b895ff9d7
t-ube <ube@coder.okinawa.jp>
2024-12-02 01:49:20 +0900

    bugfix

--- xrpl_rich_list_loader.py
+++ xrpl_rich_list_loader.py
@@ -226,7 +226,7 @@ class SupabaseUploader:
                     WHEN label LIKE 'Binance%' THEN 'Binance'
                     WHEN label LIKE 'WhiteBIT%' THEN 'WhiteBIT'
                     WHEN label LIKE 'CoinCola%' THEN 'CoinCola'
-                    WHEN label LIKE 'gatehub%' THEN 'gatehub'
+                    WHEN label LIKE '%gatehub%' THEN 'gatehub'
                     ELSE REGEXP_REPLACE(
                         REGEXP_REPLACE(label, '^~', ''),
                         '\s*\([0-9]+\)$', ''
@@ -247,7 +247,7 @@ class SupabaseUploader:
                     WHEN label LIKE 'Binance%' THEN 'Binance'
                     WHEN label LIKE 'WhiteBIT%' THEN 'WhiteBIT'
                     WHEN label LIKE 'CoinCola%' THEN 'CoinCola'
-                    WHEN label LIKE 'gatehub%' THEN 'gatehub'
+                    WHEN label LIKE '%gatehub%' THEN 'gatehub'
                     ELSE REGEXP_REPLACE(
                         REGEXP_REPLACE(label, '^~', ''),
                         '\s*\([0-9]+\)$', ''
