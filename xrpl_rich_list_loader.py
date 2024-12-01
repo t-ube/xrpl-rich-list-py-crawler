304f6b7ffb974e22aa6a31a492bd08da37be1abe
t-ube <ube@coder.okinawa.jp>
2024-12-01 11:44:30 +0900

    bugfix

--- xrpl_rich_list_loader.py
+++ xrpl_rich_list_loader.py
@@ -29,10 +29,7 @@ class XRPLRichListScraper:
         self.url = "https://xrpscan.com/balances"
         options = webdriver.ChromeOptions()
         options.add_argument('--headless=new')
-        options.add_argument('--disable-gpu')
         options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-        options.add_argument('--window-size=1920,1080')
         self.driver = webdriver.Chrome(options=options)
         self.driver.implicitly_wait(20)
 
