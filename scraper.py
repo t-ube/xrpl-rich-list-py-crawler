448d0084b1885e9bb5c4448fbb9f1d1d2164c37e
t-ube <ube@coder.okinawa.jp>
2024-12-05 23:30:00 +0900

    update

--- scraper.py
+++ scraper.py
@@ -9,7 +9,7 @@ from dataclasses import dataclass
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait, Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
